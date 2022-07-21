53af8c019a0a4b0a9270d21bafd9bbaa710b71c4
Андрей Куликов <a.kulikov1711@gmail.com>
2022-07-21 21:26:39 +1000

    ver_3.4

--- homework.py
+++ homework.py
@@ -18,6 +18,10 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
